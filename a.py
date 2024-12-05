b7cf45ee0b272616a9f14db1ec2cd5d018a8402e
likith sk <likithskgowda@gmail.com>
2024-12-05 15:46:11 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,20 +1,6 @@
-print("glad i joined staragile")
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zepto.js Example</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/zepto/1.2.0/zepto.min.js"></script>
-</head>
-<body>
-    <button id="clickButton">Click Me!</button>
-    <div id="message" style="display: none;">You clicked the button!</div>
-
-    <script>
-        $('#clickButton').on('click', function() {
-            $('#message').show();
-        });
-    </script>
-</body>
-</html>
+a=12
+b=26
+sub=a-b
+print(sub)
+print("subtaction program")
+print("= is an assigment opertion")
